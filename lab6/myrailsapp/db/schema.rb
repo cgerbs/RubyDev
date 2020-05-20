f2567b84c087bf0456c91aad223aec5183c3dcb3
c3upz <cgerbino@scu.edu>
2020-05-20 08:31:11 -0700

    added missing column

--- lab6/myrailsapp/db/schema.rb
+++ lab6/myrailsapp/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_20_061539) do
+ActiveRecord::Schema.define(version: 2020_05_20_152810) do
 
   create_table "assignments", force: :cascade do |t|
     t.string "description"
